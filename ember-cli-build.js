b715d801c90d3a6908704f1d998176761aca010f
JimXing <xyf.jim@gmail.com>
2016-03-03 23:11:54 +0800

    initial set up

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,48 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+      destDir: 'assets'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+      destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+      destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+      destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+      destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+      destDir: 'fonts'
+  });
+
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+  app.import('bower_components/font-awesome/css/font-awesome.css')
+  app.import('bower_components/font-awesome/css/font-awesome.css.map', {
+    destDir: 'assets'
+  })
+
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {
+    destDir: 'fonts'
+  })
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {
+    destDir: 'fonts'
+  })
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {
+    destDir: 'fonts'
+  })
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {
+    destDir: 'fonts'
+  })
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', {
+    destDir: 'fonts'
+  })
+
   return app.toTree();
 };
